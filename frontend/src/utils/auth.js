54887b13534d09a226f89f57e7b9b3665563f594
nikogriffs <nikogriffs@yandex.ru>
2021-08-26 12:37:12 +0700

    fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -11,11 +11,11 @@ export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
         credentials: 'include',
-        headers: {
-            'Accept': 'application/json',
-            // 'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
+        // headers: {
+        //     'Accept': 'application/json',
+        //     // 'Content-Type': 'application/json'
+        // },
+        // body: JSON.stringify({ email, password })
     })
         .then(checkAnswer)
 };
@@ -24,11 +24,11 @@ export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         credentials: 'include',
-        headers: {
-            'Accept': 'application/json',
-            // 'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
+        // headers: {
+        //     'Accept': 'application/json',
+        //     // 'Content-Type': 'application/json'
+        // },
+        // body: JSON.stringify({ email, password })
     })
         .then(checkAnswer)
 };
