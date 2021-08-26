8e18173745da24f83cba6dfd7537ce2d3fc0e344
nikogriffs <nikogriffs@yandex.ru>
2021-08-26 14:04:49 +0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
-// app.options('*', cors());
+app.options('*', cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
