dfef798b5aaf44abf9ea9579a1587470b93be48d
nikogriffs <nikogriffs@yandex.ru>
2021-08-26 09:12:34 +0700

    back 1

--- backend/app.js
+++ backend/app.js
@@ -25,8 +25,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
-
+app.options('*', cors());
 // app.use(function (req, res, next) {
 //   const { origin } = req.headers;
 //   if (allowedCors.includes(origin)) {
@@ -42,9 +44,6 @@ app.use(cors({ origin: true }));
 
 //   return res.end();
 // }
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
